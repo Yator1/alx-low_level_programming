c22dc2ad61678bfdf9a72a99fe0f4930da9e5a4a
Yator1 <titusyatz17@gmail.com>
2023-08-16 08:52:41 -0700

    It is all about debugging

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,14 +1,7 @@
-#include"main.h"
-/**
- * main-tests function that prints if integer is positive or negative
- * Return: 0
- */
-int main(void)
-{
-	int i;
+#ifndef MAIN_H
+#define MAIN_H
+#include<stdio.h>
 
-	i = 98;
-	positive_or_negative(i);
+void positive_or_negative(int i);
 
-	return (0);
-}
+#endif

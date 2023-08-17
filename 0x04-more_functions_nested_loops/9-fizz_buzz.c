0d4a2f5303e3605755e7549b64f72c17339f0ca6
Yator1 <titusyatz17@gmail.com>
2023-08-17 12:08:55 -0700

    Replacement of some numbers

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main- prints number 0 - 100
+ * for multiples of 3 it prints Fizz and
+ * for multiples of 5 it prints Buzz
+ * Return: 0
+ */
+
+int main(void)
+{
+	fizz_buzz();
+
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 != 0)
+		{
+			printf(" Fizz");
+		}
+		else if (i % 5 == 0 && i % 3 != 0)
+		{
+			printf(" Buzz");
+		}
+		else if (i % 5 == 0 && i % 3 == 0)
+		{
+			printf(" FizzBuzz");
+		}
+		else
+		{
+			printf(" %d", i);
+		}
+	}
+	printf("\n");
+
+	return (0);
+}

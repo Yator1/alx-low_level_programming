6a5552b822b88825cfcb23d6a3672095b2b0d139
Yator1 <titusyatz17@gmail.com>
2023-08-21 07:38:56 -0700

    printing a string followed by a new line

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,3 +1,19 @@
 #include"main.h"
 /**
- *
+ * _puts- a functions that prints a string followed by a new line
+ * tp stdout
+ * @str: input string
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}

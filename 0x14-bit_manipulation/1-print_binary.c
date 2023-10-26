7d833fd673f0239980e0086b3838111c17241fb4
Yator1 <titusyatz17@gmail.com>
2023-10-26 06:16:43 -0700

    Task 1 Number to Binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * print_binary - function that prints binary rep of a num
+ * @n: the number
+ *
+ * Return: void
+ */
+
+void print_binary(unsigned long int n)
+{
+	int bitSize = sizeof(n) * 8;
+	unsigned int mask = 1u << (bitSize - 1);
+	int print = 0;
+
+	while (mask > 0)
+	{
+		if (n & mask)
+		{
+			_putchar('1');
+			print = 1;
+		}
+		else if (print)
+		{
+			_putchar('0');
+		}
+
+		mask >>= 1;
+	}
+	if (!print)
+		_putchar('0');
+}
